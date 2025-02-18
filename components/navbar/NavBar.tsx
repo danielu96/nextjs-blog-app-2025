1670a7a922cc9a5860cf18817cf762cdfc91bed8
Daniel <danielu96@wp.pl>
2025-02-18 12:57:18 +0100

    Add Frame14122 component to NavBar

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Logo from './Logo';
+import Frame14122 from './Frame141222';
 
 
 function Navbar() {
     return (
         <>
             <Logo />
+            <div className="hideOnSmall"> {/* Hide on small screens */}
+                <Frame14122 />
+            </div>
         </>
     )
 
