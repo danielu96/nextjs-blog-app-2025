c53813aaa1ddd1f2e0f9437c84f9e48d99cc3f66
Daniel <danielu96@wp.pl>
2025-02-19 13:29:33 +0100

    Add Rectangle4 component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/navbar/NavBar";
 import Header from "@/components/header/Header";
+import Rectangle4 from "@/components/home/Rectangle4";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Header />
+      <Rectangle4 />
     </>
   );
 }
