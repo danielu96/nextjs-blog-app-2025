db5f0423ca3cceab577401973f7a50e554ebf5c9
Daniel <danielu96@wp.pl>
2025-02-19 18:46:28 +0100

    Add useSearchParams to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
+"use client"
 import Navbar from "@/components/navbar/NavBar";
 import Header from "@/components/header/Header";
 import Rectangle4 from "@/components/home/Rectangle4";
+import { useSearchParams } from 'next/navigation'
 
 export default function Home() {
+  const searchParams = useSearchParams()
+  const category = searchParams.get('category')
   return (
     <>
       <Navbar />
       <Header />
-      <Rectangle4 />
+      <Rectangle4 category={category ?? undefined} />
     </>
   );
 }
