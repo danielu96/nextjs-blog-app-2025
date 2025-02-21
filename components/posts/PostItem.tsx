740fd46e7c45436b4d73c2b236c655ef43d1385b
Daniel <danielu96@wp.pl>
2025-02-21 11:51:51 +0100

    Add Link to detailPage

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -1,13 +1,20 @@
 
 'use client';
 import { PostItemProps } from "@/utils/types";
+import Link from 'next/link';
 
 
-const PostItem = ({ title, body }: PostItemProps) => {
+const PostItem = ({ id, title, body }: PostItemProps) => {
     return (
         <div className="card">
             <h2 className="text-2xl font-bold mt-2 text-slate-800 h-10">{title}</h2>
             <p className="mt-36 h-24">{body.substring(0, 200)}...</p>
+            <div className="flex justify-between mt-14">
+                <Link href={`/posts/${id}`}>
+                    <span className="text-gray-950 text-xl font-bold">zobacz więcej →</span>
+                </Link>
+
+            </div>
         </div>
     );
 };
