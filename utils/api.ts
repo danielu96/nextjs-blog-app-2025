7a22d7024684a24d74aa6314f5d30d3f4812a9cb
Daniel <danielu96@wp.pl>
2025-02-22 13:28:04 +0100

    add Category

--- utils/api.ts
+++ utils/api.ts
@@ -1,11 +1,16 @@
 import axios from 'axios';
+// import { categories } from './categories';
+import { categories, Category } from './categories';
 
 const API_URL = 'https://jsonplaceholder.typicode.com';
+
+// const categories = ['WIEDZA', 'INSPIRACJE', 'INTERPRETACJE', 'DOSTĘPNE',];
 
 export const fetchPosts = async () => {
     const response = await axios.get(`${API_URL}/posts`);
     const posts = response.data.map((post: any) => ({
         ...post,
+        category: categories[Math.floor(Math.random() * categories.length)] as Category, // Jawne typowanie
     }));
     return posts;
 };
@@ -14,6 +19,7 @@ export const fetchPostDetails = async (id: number) => {
     const response = await axios.get(`${API_URL}/posts/${id}`);
     const post = {
         ...response.data,
+        category: categories[Math.floor(Math.random() * categories.length)] as Category, // Jawne typowanie
     };
     return post;
 };
