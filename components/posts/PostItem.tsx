b69f0430842574ffe2221381a5e870f74b701ca1
Daniel <danielu96@wp.pl>
2025-02-23 11:03:35 +0100

    add uppercase

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -10,7 +10,7 @@ import Link from 'next/link';
 const PostItem: React.FC<PostItemProps> = ({ id, title, body, dateAdded, category }) => {
     return (
         <div className="card">
-            <div className="mt-2 text-sm font-semibold text-gray-900 mb-2 underline">{category.label}</div>
+            <div className="mt-2 text-sm font-semibold text-gray-900 mb-2 underline uppercase">{category.label}</div>
             <h2 className="text-2xl font-bold mt-2 text-slate-800 h-10">{title}</h2>
             <div className='position relative  top-[100px]'>   {dateAdded && <p className="font-bold text-2xl text-gray-500">{new Date(dateAdded).toLocaleDateString()}</p>}</div>
             <p className="mt-36 h-24">{body.substring(0, 200)}...</p>
