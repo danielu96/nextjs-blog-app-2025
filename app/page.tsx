ab3c64b05645e9841466e98ab4ee5ec4927df179
Daniel <danielu96@wp.pl>
2025-02-23 21:18:16 +0100

    add  SortProvider to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,21 @@ import Rectangle4 from "@/components/home/Rectangle4";
 import { useSearchParams } from 'next/navigation'
 import PostList from "@/components/posts/PostList";
 import { Suspense } from "react";
+import { SortProvider } from "@/components/context/SortContext";
 
 export default function Home() {
   const searchParams = useSearchParams()
   const category = searchParams.get('category')
   return (
     <>
-      <Navbar />
-      <Header />
-      <Rectangle4 category={category ?? undefined} />
-      <Suspense fallback={<div>Loading...</div>}>
-        <PostList />
-      </Suspense>
-
+      <SortProvider>
+        <Navbar />
+        <Header />
+        <Rectangle4 category={category ?? undefined} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <PostList />
+        </Suspense>
+      </SortProvider>
     </>
   );
 }
