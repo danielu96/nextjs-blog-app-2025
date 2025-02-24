6d2c0b2baaf3426a9ac92b380bdde0dd4b0a9921
Daniel <danielu96@wp.pl>
2025-02-24 13:43:20 +0100

    add dynamic category colors based on label

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -2,7 +2,7 @@
 'use client';
 import { PostItemProps } from "@/utils/types";
 import Link from 'next/link';
-// import { Category } from './categories';
+import { categoryColors } from "@/utils/categories";
 
 
 
@@ -10,9 +10,9 @@ import Link from 'next/link';
 const PostItem: React.FC<PostItemProps> = ({ id, title, body, dateAdded, category }) => {
     return (
         <div className="card">
-            <div className="mt-2 text-sm font-semibold text-gray-900 mb-2 underline uppercase">{category.label}</div>
+            <div className={`mt-2 text-sm font-semibold ${categoryColors[category.label]} mb-2 underline uppercase`}>{category.label}</div>
             <h2 className="text-2xl font-bold mt-2 text-slate-800 h-10">{title}</h2>
-            <div className='position relative  top-[100px]'>   {dateAdded && <p className="font-bold text-2xl text-gray-500">{new Date(dateAdded).toLocaleDateString()}</p>}</div>
+            <div className='position relative  top-[100px] min-h-[32px]'>   {dateAdded && <p className="font-bold text-2xl text-gray-500">{new Date(dateAdded).toLocaleDateString()}</p>}</div>
             <p className="mt-36 h-24">{body.substring(0, 200)}...</p>
             <div className="flex justify-between mt-14">
                 <Link href={`/posts/${id}`}>
